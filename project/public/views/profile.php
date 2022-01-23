754dc16dad5e2798f232355f4901322fe552a342
Alona655 <72976145+Alona655@users.noreply.github.com>
2022-01-24 00:13:55 +0100

    Adding the info about person on a profile

--- project/public/views/profile.php
+++ project/public/views/profile.php
@@ -23,7 +23,8 @@
 
     <h1 class="titleProfile">Mój Profil</h1>
     <div class="contentProfile">
-        <img src="">
+        <div class=""></div>
+
         <div class="parametrsProfile">
             <div class="nameProfilePage">
                 <div class="name-conteinerProfilePage">
@@ -37,6 +38,35 @@
                     <p class="surProfile">Janowska</p>
                 </div>
             </div>
+            <div class="thirdLineProfile">
+                <div class="heightProfilePage">
+                    <div class="height-conteinerProfilePage">
+                        <p class="titleHeightProfile">Wzrost</p>
+                        <p class="heightProfile">1.67</p>
+                    </div>
+                </div>
+                <div class="weightProfilePage">
+                    <div class="weight-conteinerProfilePage">
+                        <p class="titleWeightProfile">Waga</p>
+                        <p class="weightProfile">58</p>
+                    </div>
+                </div>
+            </div>
+            <div class="fourthLineProfile">
+                <div class="ageProfilePage">
+                    <div class="age-conteinerProfilePage">
+                        <p class="titleAgeProfile">Wiek</p>
+                        <p class="ageProfile">22</p>
+                    </div>
+                </div>
+                <div class="goalProfilePage">
+                    <div class="goal-conteinerProfilePage">
+                        <p class="titleGoalProfile">Cel ćwiczeń</p>
+                        <p class="goalProfile">Rozciąganie</p>
+                    </div>
+                </div>
+            </div>
+
 
         </div>
     </div>
