fd55cf167b7b8536461d2e82b8ac2d44ba9de803
Alona655 <72976145+Alona655@users.noreply.github.com>
2022-01-23 17:03:54 +0100

    Adding the page of the registration and filling it

---
+++ project/public/views/registration.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/registration.css">
+    <title>Registration page</title>
+</head>
+
+<body>
+<main>
+    <header>
+        <div class="logoRegistrationPage">
+            <p>yoga</p>
+        </div>
+    </header>
+    <div class="tableRegPage">
+        <p class="titleTable">Zarejestruj się</p>
+        <div class="formTable">
+            <div class="firstLine">
+                <div class="nameRegPage">
+                    <div class="name-conteinerRegPage">
+                        <input class="conteinerNameRegPage" type="text" size="30" placeholder="Imię">
+                    </div>
+                </div>
+                <!--                <form class="plecList">-->
+                <!--                    <p>-->
+                <!--                        <select name="plec" multiple>-->
+                <!--                            <option value="s1">Kobieta</option>-->
+                <!--                            <option value="s2">Mężczyzna</option>-->
+                <!--                        </select>-->
+                <!--                    </p>-->
+                <!--                </form>-->
+            </div>
+            <div class="surnameRegPage">
+                <div class="surname-conteinerRegPage">
+                    <input class="conteinerSurRegPage" type="text" size="30" placeholder="Nazwisko">
+                </div>
+            </div>
+            <div class="birthdayRegPage">
+                <div class="birthday-conteinerRegPage">
+                    <input class="conteinerbirthdayRegPage" type="date" size="30" placeholder="Data urodzenia">
+                </div>
+            </div>
+            <div class="forthLine">
+                <div class="weight">
+                    <div class="weight-conteinerRegPage">
+                        <input class="conteinerWeightRegPage" type="number" size="30" placeholder="Waga">
+                    </div>
+                </div>
+                <div class="height">
+                    <div class="height-conteinerRegPage">
+                        <input class="conteinerHeightRegPage" type="number" size="30" placeholder="Wzrost">
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
+</main>
+
+<footer>
+
+</footer>
+</body>
