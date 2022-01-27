1cba7a56ae6683dc6ec20d1c9613849c4ef39639
Alona655 <72976145+Alona655@users.noreply.github.com>
2022-01-27 16:45:02 +0100

    Start to filling My Course page

---
+++ project/public/views/myCourse.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/myCourse.css">
+    <title>My Courses</title>
+</head>
+
+<body>
+<main>
+    <header>
+        <div class="logoCourse">
+            <p>yoga</p>
+        </div>
+        <div class="menuCourse">
+            <nav class="navCourse">
+                <a class="findingCourseCourse" href="">Poszuk kursu</a>
+                <a class="aboutUsCourse" href="">O nas</a>
+                <a class="myProfileInCourse" href="">Mój Profil</a>
+                <a class="burgerCourse" href=""><img src="public/img/burger.svg"></a>
+            </nav>
+        </div>
+    </header>
+
+</main>
+
+
+
+</body>
