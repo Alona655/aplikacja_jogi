09a8ccf9b57a7b6dca4d434a738c81c6c01dff56
Alona655 <72976145+Alona655@users.noreply.github.com>
2022-01-27 20:03:09 +0100

    Starting working on Settings page

---
+++ project/public/views/settings.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/settings.css">
+    <title>Settings</title>
+</head>
+
+<body>
+<main>
+    <header>
+        <div class="logoSettings">
+            <p>yoga</p>
+        </div>
+        <div class="menuSettings">
+            <nav class="navSettings">
+                <a class="findingCourseSettings" href="">Poszuk kursu</a>
+                <a class="aboutUsSettings" href="">O nas</a>
+                <a class="myProfileInSettings" href="">Mój Profil</a>
+                <a class="burgerSettings" href=""><img src="public/img/burger.svg"></a>
+            </nav>
+        </div>
+    </header>
+    <h1 class="titleSettings">Moje kursy</h1>
+
+</main>
+</body>
