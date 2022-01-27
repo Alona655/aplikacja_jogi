fa54cb350c31cc7b2d6715b53fad51c5a45d31d5
Alona655 <72976145+Alona655@users.noreply.github.com>
2022-01-27 16:04:25 +0100

    Filling Activity page

--- project/public/views/activity.php
+++ project/public/views/activity.php
@@ -28,20 +28,52 @@
         background-position-x: right;">
             <div class="conteinerActivityPage">
                 <p class="countPerWeek"><span class="titlePerWeek">5 </span>Zajęć za tydzień</p>
-<!--                <img src="public/imgActivity/activity.svg">-->
             </div>
         </div>
 
-        <div class="perMonth">
-
+        <div class="perMonth" style="
+        background-image: url(&quot;public/imgActivity/activity.svg&quot;);
+        background-repeat: no-repeat;
+        background-position-x: right;">
+                <div class="conteinerActivityPage">
+                    <p class="countPerMonth"><span class="titlePerMonth">20 </span>Zajęć za miesiąc</p>
+                </div>
         </div>
 
         <div class="calendarActivity">
-
+            <form class="calendarActivity"></form>
         </div>
     </div>
 
 </main>
+
+<footer>
+    <div class="titleFooterActivity">
+        <p>yoga</p>
+    </div>
+    <div class="contactsActivity">
+        <div class="profileActivity">
+            <a>Mój profil</a>
+        </div>
+        <div class="aboutUsActivity">
+            <a>O nas</a>
+        </div>
+        <div class="contactsActivity_1">
+            <a>Kontakty</a>
+        </div>
+    </div>
+    <div class="mediaActivity">
+        <img src="facebookBlock7.svg">
+        <img src="instagramBlock7.svg">
+        <img src="viberBlock7.svg">
+        <img src="twitterBlock7.svg">
+    </div>
+    <div class="termAndPrivacyActivity">
+        <p class="termActivity">Terms & Conditions</p>
+        <p class="privacyActivity">Privacy Policy</p>
+    </div>
+    <p class="copyrigthActivity">Copyrigth © 2020 Flaunter, Ltd. All rights reserved. Site credit.</p>
+</footer>
 
 
 </body>
