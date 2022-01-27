da228db613f8f91cbed36df7dcd73c69a9a3ed62
Alona655 <72976145+Alona655@users.noreply.github.com>
2022-01-27 21:53:03 +0100

    Filling Settings page

--- project/public/views/settings.php
+++ project/public/views/settings.php
@@ -20,7 +20,29 @@
             </nav>
         </div>
     </header>
-    <h1 class="titleSettings">Moje kursy</h1>
+    <h1 class="titleSettings">Ustawienia</h1>
+    <div class="buttonRegSettings">
+        <button class="textButtonSettings">Edytuj profil</button>
+    </div>
+    <p class="changePassword">Zmień hasło</p>
+    <div class="oldPasswordSet">
+        <div class="oldPassword-conteinerSetPage">
+            <input class="oldPassword-conteiner-formSetPage" type="password" size="30" placeholder="Stare hasło">
+        </div>
+    </div>
+    <div class="newPasswordSet">
+        <div class="newPassword-conteinerSetPage">
+            <input class="newPassword-conteiner-formSetPage" type="password" size="30" placeholder="Nowe hasło">
+        </div>
+    </div>
+    <div class="buttonChangePasswordSettings">
+        <button class="textButtonChangePasswordSettings">Zmień</button>
+    </div>
+
+    <div class="buttonDeleteProfileSettings">
+        <button class="textButtonDeleteProfileSettings">Usuń profil</button>
+    </div>
+
 
 </main>
 </body>
