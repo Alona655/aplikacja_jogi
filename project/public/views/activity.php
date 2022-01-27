6a5297b4debc07decedb268a0d49653584a7f3e2
Alona655 <72976145+Alona655@users.noreply.github.com>
2022-01-27 15:45:13 +0100

    Filling Activity page

--- project/public/views/activity.php
+++ project/public/views/activity.php
@@ -21,6 +21,25 @@
     </div>
     </header>
     <h1 class="titleActivity">Moja aktywność</h1>
+    <div class="contentActivity">
+        <div class="perWeek" style="
+        background-image: url(&quot;public/imgActivity/activity.svg&quot;);
+        background-repeat: no-repeat;
+        background-position-x: right;">
+            <div class="conteinerActivityPage">
+                <p class="countPerWeek"><span class="titlePerWeek">5 </span>Zajęć za tydzień</p>
+<!--                <img src="public/imgActivity/activity.svg">-->
+            </div>
+        </div>
+
+        <div class="perMonth">
+
+        </div>
+
+        <div class="calendarActivity">
+
+        </div>
+    </div>
 
 </main>
 
