11bab681a39e1936978b82a01769ca007ba5f133
Alona655 <72976145+Alona655@users.noreply.github.com>
2022-01-27 18:21:22 +0100

    Filling My Course page

--- project/public/views/myCourse.php
+++ project/public/views/myCourse.php
@@ -20,6 +20,50 @@
             </nav>
         </div>
     </header>
+    <h1 class="titleCourse">Moje kursy</h1>
+    <div class="contentCourse">
+        <div class="video1">
+            <iframe width="476" height="267.75" src="https://www.youtube.com/embed/tt6efl4ISvA?rel=0"
+                    srcdoc="<style>*{padding:0;margin:0;overflow:hidden}
+    html,body{height:100%}
+    img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}
+    span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}
+    </style>
+     <a href=https://www.youtube.com/embed/tt6efl4ISvA?rel=0>
+    <img src=https://img.youtube.com/vi/tt6efl4ISvA/hqdefault.jpg alt='Demo video'>
+    <span>▶</span>
+    </a>"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                    title="Demo video"
+                    >
+            </iframe>
+            <p class="titleVide1">Morning Yoga - 10 min Full Body Yoga at Home Yoga Flow for Beginners</p>
+        </div>
+
+        <div class="video2">
+            <iframe width="476" height="267.75" src="https://www.youtube.com/embed/VaoV1PrYft4?rel=0"
+                    srcdoc="<style>*{padding:0;margin:0;overflow:hidden}
+    html,body{height:100%}
+    img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}
+    span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}
+    </style>
+     <a href=https://www.youtube.com/embed/VaoV1PrYft4?rel=0>
+    <img src=https://img.youtube.com/vi/VaoV1PrYft4/hqdefault.jpg alt='Demo video'>
+    <span>▶</span>
+    </a>"
+
+                    title="YouTube video player" frameborder="0" allow="accelerometer;
+                    autoplay;
+                    clipboard-write;
+                    encrypted-media;
+                    gyroscope;
+                    picture-in-picture" allowfullscreen></iframe>
+
+            <p class="titleVide2">10 minute Morning Yoga for Beginners</p>
+        </div>
+    </div>
 
 </main>
 
--- project/public/views/profile.php
+++ project/public/views/profile.php
@@ -2,7 +2,7 @@
 
 <head>
     <link rel="stylesheet" type="text/css" href="public/css/profile.css">
-    <title>Profile</title>
+    <title>My profile</title>
 </head>
 
 <body>
