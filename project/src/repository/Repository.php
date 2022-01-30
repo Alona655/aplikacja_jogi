36c3a3497cf4f92ac63804ac2d65d2f1ee5e5ea1
Alona655 <72976145+Alona655@users.noreply.github.com>
2022-01-30 19:10:37 +0100

    Working with database, implemented in the project

--- project/src/repository/Repository.php
+++ project/src/repository/Repository.php
@@ -2,13 +2,11 @@
 
 require_once __DIR__.'/../../Database.php';
 
-class Repository
-{
+class Repository {
     protected $database;
 
     public function __construct()
     {
         $this->database = new Database();
     }
-
 }
